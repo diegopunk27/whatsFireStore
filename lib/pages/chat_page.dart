4dea1f263c7a4a9a511ad5553883cd0900e2558b
Diegoy <diegopunk27@hotmail.com>
2021-07-12 22:32:55 -0300

    Cambios en chat_page
    - Se agregó estilo a la caja de mensajes similar a whatsapp.
    - Se creó un widgets en base a esa caja de texto

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,12 +4,44 @@ import 'package:whatsfire/models/groups.dart';
 import 'package:whatsfire/models/message.dart';
 import 'package:whatsfire/widgets/circular_progress.dart';
 import 'package:whatsfire/widgets/error_center.dart';
+import 'package:whatsfire/widgets/message_box.dart';
 import 'package:whatsfire/widgets/messages_list.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  bool changeInput;
+  TextEditingController controller;
+  @override
+  void initState() {
+    controller = TextEditingController();
+    changeInput = false;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Groups grupo = ModalRoute.of(context).settings.arguments;
+    controlChangeText().listen((val) {
+      if (val) {
+        setState(() {
+          changeInput = true;
+        });
+      } else {
+        setState(() {
+          changeInput = false;
+        });
+      }
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text(grupo.name),
@@ -24,10 +56,34 @@ class ChatPage extends StatelessWidget {
             return CircularProgress();
           }
           if (snapshot.hasData) {
-            return MessagesList(mensajes: snapshot.data);
+            return Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 20),
+                    child: MessagesList(mensajes: snapshot.data),
+                  ),
+                ),
+                MessageBox(
+                  controller: controller,
+                  changeInput: changeInput,
+                  onSend: (text) {
+                    print(text);
+                  },
+                ),
+              ],
+            );
           }
         },
       ),
     );
+  }
+
+  Stream<bool> controlChangeText() async* {
+    if (controller.text.trim() == "") {
+      yield false;
+    } else if (controller.text.trim() != "") {
+      yield true;
+    }
   }
 }
---
+++ lib/widgets/message_box.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class MessageBox extends StatelessWidget {
+  const MessageBox({
+    @required this.controller,
+    @required this.changeInput,
+    @required this.onSend,
+  });
+
+  final TextEditingController controller;
+  final bool changeInput;
+  final Function onSend;
+
+  void _send(String text) {
+    onSend(text);
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 0, 0, 5),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  style: BorderStyle.solid,
+                  color: Colors.teal,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.horizontal(
+                    left: Radius.circular(50), right: Radius.circular(50)),
+              ),
+              padding: EdgeInsets.only(left: 20),
+              child: TextField(
+                controller: controller,
+                onSubmitted: _send,
+                cursorColor: Colors.teal,
+                decoration: InputDecoration(
+                  hintText: "Esscriba un mensaje...",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                _send(controller.text);
+              },
+              child: Container(
+                width: 45,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.teal,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  changeInput ? Icons.send : Icons.mic,
+                  color: Colors.white,
+                  size: 25,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
